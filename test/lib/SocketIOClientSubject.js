b5c7b236fbf610375eaed65681de87bc4999b50a
Brian Vanderbusch <chief@hackerhappyhour.com>
2016-05-14 17:20:39 -0500

    fixes from linter

--- test/lib/SocketIOClientSubject.js
+++ test/lib/SocketIOClientSubject.js
@@ -3,4 +3,4 @@ import {SocketIOClientSubject} from '../../lib/SocketIOClientSubject';
 var config = {
   url: 'http://localhost:3031'
 }
-var source = new SocketIOClientSubject(config);
+new SocketIOClientSubject(config);
--- test/server.js
+++ test/server.js
@@ -1,7 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var Observable = require('rxjs/Observable').Observable;
 
 io.on('connection', function(socket){
   console.log('a client connected');

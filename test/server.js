c7ba303c86d4021a819a33bf542654533d79fb1c
Brian Vanderbusch <chief@hackerhappyhour.com>
2016-05-15 20:01:29 -0500

    binary type detection

--- test/server.js
+++ test/server.js
@@ -1,12 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
+var path = require('path');
 
 io.on('connection', function(socket){
   console.log('a client connected');
   socket.emit("testing", {name: "testing"});
   socket.on("thing", function(packet){
     console.log("thing: ", packet.data);
+  });
+
+  fs.readFile(path.join(__dirname, '../package.json'), function(err, buf){
+    socket.emit(buf);
   })
 });
 

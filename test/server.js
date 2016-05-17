612723719ed618d5867f198c842cb637d408f8d6
Brian Vanderbusch <brian.vanderbusch.qsho@statefarm.com>
2016-05-17 07:20:02 -0500

    initial ServerSubject

--- test/server.js
+++ test/server.js
@@ -1,23 +1,16 @@
+require('babel-register');
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var RxSocketServer = require('../lib/WebSocketServer').WebSocketServer;
 var fs = require('fs');
 var path = require('path');
 
-io.on('connection', function(socket){
-  io.on('packet', function(packet){
-    console.log('packet: ', packet);
-  });
-  console.log('a client connected');
-  socket.emit("testing", {name: "testing"});
-  socket.on("thing", function(packet){
-    console.log("thing: ", packet.data);
-  });
-
-  fs.readFile(path.join(__dirname, '../package.json'), function(err, buf){
-    socket.emit(buf);
-  })
+var socketServer = new RxSocketServer(http);
+socketServer.subscribe(function(data){
+  console.log(data)
 });
+
+socketServer.next('testing');
 
 http.listen('3031', function(){
   console.log('listening on :3031');
